ce8a103ccd927bf0f6e45a3d5169ed741a50f90f
tgless <126119481+tgless@users.noreply.github.com>
2026-05-06 14:48:58 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
